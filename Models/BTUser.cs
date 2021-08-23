b863684e9c62c9fd2430e4de5f269f54c165bb1c
elbiesmith <elbie.smith@gmail.com>
2021-08-23 15:59:19 -0500

    Removed sql migrations, and started BTUser Model

---
+++ Models/BTUser.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Titan_BugTracker.Models
+{
+    public class BTUser : IdentityUser
+    {
+        [Required]
+        [DisplayName("First Name")]
+        [StringLength(40, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 2)]
+        public string FirstName { get; set; }
+
+        [Required]
+        [DisplayName("Last Name")]
+        [StringLength(40, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 2)]
+        public string LastName { get; set; }
+
+        [NotMapped]
+        [DisplayName("FullName")]
+        public string FullName { get { return $"{FirstName} {LastName}"; } }
+
+        [NotMapped]
+        [DataType(DataType.Upload)]
+        public IFormFile AvatarFormFile { get; set; }
+
+        [DisplayName("Avatar")]
+        public string AvatarFileName { get; set; }
+
+        public byte[] AvatarFileData { get; set; }
+
+        [DisplayName("File Extension")]
+        public string AvatarContentType { get; set; }
+    }
+}
