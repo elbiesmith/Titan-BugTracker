3139bc3fd72d311e6b643f35686e00bd5b896759
elbiesmith <elbie.smith@gmail.com>
2021-08-25 14:11:54 -0500

    Create BTCompanyInfoService.cs

---
+++ Services/BTCompanyInfoService.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Titan_BugTracker.Models;
+using Titan_BugTracker.Services.Interfaces;
+
+namespace Titan_BugTracker.Services
+{
+    public class BTCompanyInfoService : IBTCompanyInfoService
+    {
+        public Task<List<BTUser>> GetAllMembersAsync(int companyId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Project>> GetAllProjectsAsync(int companyId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Ticket>> GetAllTicketsAsync(int companyId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Company> GetCompanyInfoByIdAsync(int? companyId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
