d779b0b538e3029f4eed9973677206099c83b06b
elbiesmith <elbie.smith@gmail.com>
2021-08-25 11:08:25 -0500

    Create BTRolesService.cs

---
+++ Services/BTRolesService.cs
@@ -0,0 +1,100 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Titan_BugTracker.Data;
+using Titan_BugTracker.Models;
+using Titan_BugTracker.Services.Interfaces;
+
+namespace Titan_BugTracker.Services
+{
+    public class BTRolesService : IBTRolesService
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly RoleManager<IdentityRole> _roleManager;
+        private readonly UserManager<BTUser> _userManager;
+
+        public BTRolesService(ApplicationDbContext context,
+                              RoleManager<IdentityRole> roleManager,
+                              UserManager<BTUser> userManager)
+        {
+            _context = context;
+            _roleManager = roleManager;
+            _userManager = userManager;
+        }
+
+        public async Task<bool> AddUserToRoleAsync(BTUser user, string roleName)
+        {
+            try
+            {
+                bool result = (await _userManager.AddToRoleAsync(user, roleName)).Succeeded;
+                return result;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        public async Task<string> GetRoleNameByIdAsync(string roleId)
+        {
+            try
+            {
+                IdentityRole role = _context.Roles.Find(roleId);
+                // string result = role.Name;
+                string result = await _roleManager.GetRoleNameAsync(role);
+
+                return result;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        public async Task<List<IdentityRole>> GetRolesAsync()
+        {
+            try
+            {
+                List<IdentityRole> result = await _context.Roles.ToListAsync();
+                return result;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        public Task<IEnumerable<string>> GetuserRolesAsync(BTUser user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<BTUser>> GetUsersInRoleAsync(string roleName, int companyId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<BTUser>> GetUsersNotInRoleAsync(string roleName, int companyId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> IsUserInRoleAsync(BTUser user, string roleName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> RemoveUserFromRoleAsync(BTUser user, string roleName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> RemoveUserFromRolesAsync(BTUser user, IEnumerable<string> roles)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
