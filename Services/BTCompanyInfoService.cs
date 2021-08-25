5ad6185a64d55959b43242d2de94106ac92fcfd9
elbiesmith <elbie.smith@gmail.com>
2021-08-25 14:43:32 -0500

    Update BTCompanyInfoService.cs

--- Services/BTCompanyInfoService.cs
+++ Services/BTCompanyInfoService.cs
@@ -1,7 +1,9 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Titan_BugTracker.Data;
 using Titan_BugTracker.Models;
 using Titan_BugTracker.Services.Interfaces;
 
@@ -9,24 +11,101 @@ namespace Titan_BugTracker.Services
 {
     public class BTCompanyInfoService : IBTCompanyInfoService
     {
-        public Task<List<BTUser>> GetAllMembersAsync(int companyId)
+        private readonly ApplicationDbContext _context;
+
+        public BTCompanyInfoService(ApplicationDbContext context)
         {
-            throw new NotImplementedException();
+            _context = context;
         }
 
-        public Task<List<Project>> GetAllProjectsAsync(int companyId)
+        public async Task<List<BTUser>> GetAllMembersAsync(int companyId)
         {
-            throw new NotImplementedException();
+            List<BTUser> result = new();
+            try
+            {
+                result = await _context.Users.Where(u => u.CompanyId == companyId).ToListAsync();
+
+                return result;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
         }
 
-        public Task<List<Ticket>> GetAllTicketsAsync(int companyId)
+        public async Task<List<Project>> GetAllProjectsAsync(int companyId)
         {
-            throw new NotImplementedException();
+            List<Project> result = new();
+
+            try
+            {
+                result = await _context.Projects.Where(p => p.CompanyId == companyId)
+                                                .Include(p => p.Members)
+                                                .Include(p => p.Tickets)
+                                                    .ThenInclude(t => t.Comments)
+                                                .Include(p => p.Tickets)
+                                                    .ThenInclude(t => t.Attachments)
+                                                .Include(p => p.Tickets)
+                                                    .ThenInclude(t => t.History)
+                                                .Include(p => p.Tickets)
+                                                    .ThenInclude(t => t.Notifications)
+                                                .Include(p => p.Tickets)
+                                                    .ThenInclude(t => t.DeveloperUser)
+                                                .Include(p => p.Tickets)
+                                                    .ThenInclude(t => t.OwnerUser)
+                                                .Include(p => p.Tickets)
+                                                    .ThenInclude(t => t.TicketStatus)
+                                                .Include(p => p.Tickets)
+                                                    .ThenInclude(t => t.TicketPriority)
+                                                .Include(p => p.Tickets)
+                                                    .ThenInclude(t => t.TicketType)
+                                                .Include(p => p.ProjectPriority)
+                                                .ToListAsync();
+
+                return result;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
         }
 
-        public Task<Company> GetCompanyInfoByIdAsync(int? companyId)
+        public async Task<List<Ticket>> GetAllTicketsAsync(int companyId)
         {
-            throw new NotImplementedException();
+            List<Ticket> result = new();
+
+            try
+            {
+                result = await _context.Tickets.Where(t => t.Project.CompanyId == companyId).ToListAsync();
+                return result;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        public async Task<Company> GetCompanyInfoByIdAsync(int? companyId)
+        {
+            Company result = new();
+
+            try
+            {
+                if (companyId != null)
+                {
+                    result = await _context.Companies
+                                           .Include(c => c.Members)
+                                           .Include(c => c.Projects)
+                                           .Include(c => c.Invites)
+                                           .FirstOrDefaultAsync(c => c.Id == companyId);
+                }
+
+                return result;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
         }
     }
 }
