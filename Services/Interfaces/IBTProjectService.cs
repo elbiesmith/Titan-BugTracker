5f510119d717bf6bbda588c9b477ed137f19ef80
elbiesmith <elbie.smith@gmail.com>
2021-08-25 15:35:38 -0500

    BTProjectService and Interface added

---
+++ Services/Interfaces/IBTProjectService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Titan_BugTracker.Services.Interfaces
+{
+    public interface IBTProjectService
+    {
+    }
+}
