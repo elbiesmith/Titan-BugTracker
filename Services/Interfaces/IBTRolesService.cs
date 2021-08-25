eaa591941d5f8d62e3bcd92ebc3098e112db09be
elbiesmith <elbie.smith@gmail.com>
2021-08-25 10:02:25 -0500

    added links to shared nav, and Roles service interface

---
+++ Services/Interfaces/IBTRolesService.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Titan_BugTracker.Models;
+
+namespace Titan_BugTracker.Services.Interfaces
+{
+    public interface IBTRolesService
+    {
+        public Task<bool> AddUserToRoleAsync(BTUser user, string roleName);
+
+        public Task<List<IdentityRole>> GetRolesAsync();
+
+        public Task<string> GetRoleNameByIdAsync(string roleId);
+
+        public Task<List<BTUser>> GetUsersInRoleAsync(string roleName, int companyId);
+
+        public Task<List<BTUser>> GetUsersNotInRoleAsync(string roleName, int companyId);
+
+        public Task<IEnumerable<string>> GetuserRolesAsync(BTUser user);
+
+        public Task<bool> IsUserInRoleAsync(BTUser user, string roleName);
+
+        public Task<bool> RemoveUserFromRoleAsync(BTUser user, string roleName);
+
+        public Task<bool> RemoveUserFromRolesAsync(BTUser user, IEnumerable<string> roles);
+    }
+}
