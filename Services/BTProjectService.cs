a379e973061f166a24d0f9eeed56e335c888dbf6
elbiesmith <elbie.smith@gmail.com>
2021-08-25 15:42:14 -0500

    Update BTProjectService.cs

--- Services/BTProjectService.cs
+++ Services/BTProjectService.cs
@@ -9,109 +9,235 @@ namespace Titan_BugTracker.Services
 {
     public class BTProjectService : IBTProjectService
     {
-        public Task AddNewProjectAsync(Project project)
+        public async Task AddNewProjectAsync(Project project)
         {
-            throw new NotImplementedException();
+            try
+            {
+            }
+            catch (Exception)
+            {
+                throw;
+            }
         }
 
-        public Task<bool> AddProjectManagerAsync(string userId, int projectId)
+        public async Task<bool> AddProjectManagerAsync(string userId, int projectId)
         {
-            throw new NotImplementedException();
+            try
+            {
+            }
+            catch (Exception)
+            {
+                throw;
+            }
         }
 
-        public Task<bool> AddUserToProjectAsync(string userId, int projectId)
+        public async Task<bool> AddUserToProjectAsync(string userId, int projectId)
         {
-            throw new NotImplementedException();
+            try
+            {
+            }
+            catch (Exception)
+            {
+                throw;
+            }
         }
 
-        public Task ArchiveProjectAsync(Project project)
+        public async Task ArchiveProjectAsync(Project project)
         {
-            throw new NotImplementedException();
+            try
+            {
+            }
+            catch (Exception)
+            {
+                throw;
+            }
         }
 
-        public Task<List<BTUser>> GetAllProjectMembersExceptPMAsync(int projectId)
+        public async Task<List<BTUser>> GetAllProjectMembersExceptPMAsync(int projectId)
         {
-            throw new NotImplementedException();
+            try
+            {
+            }
+            catch (Exception)
+            {
+                throw;
+            }
         }
 
-        public Task<List<Project>> GetAllProjectsByCompany(int companyId)
+        public async Task<List<Project>> GetAllProjectsByCompany(int companyId)
         {
-            throw new NotImplementedException();
+            try
+            {
+            }
+            catch (Exception)
+            {
+                throw;
+            }
         }
 
-        public Task<List<Project>> GetAllProjectsByPriority(int companyId, string priorityName)
+        public async Task<List<Project>> GetAllProjectsByPriority(int companyId, string priorityName)
         {
-            throw new NotImplementedException();
+            try
+            {
+            }
+            catch (Exception)
+            {
+                throw;
+            }
         }
 
-        public Task<List<Project>> GetArchivedProjectsByCompany(int companyId)
+        public async Task<List<Project>> GetArchivedProjectsByCompany(int companyId)
         {
-            throw new NotImplementedException();
+            try
+            {
+            }
+            catch (Exception)
+            {
+                throw;
+            }
         }
 
-        public Task<List<BTUser>> GetDevelopersOnProjectAsync(int projectId)
+        public async Task<List<BTUser>> GetDevelopersOnProjectAsync(int projectId)
         {
-            throw new NotImplementedException();
+            try
+            {
+            }
+            catch (Exception)
+            {
+                throw;
+            }
         }
 
-        public Task<Project> GetProjectByIdAsync(int projectId, int companyId)
+        public async Task<Project> GetProjectByIdAsync(int projectId, int companyId)
         {
-            throw new NotImplementedException();
+            try
+            {
+            }
+            catch (Exception)
+            {
+                throw;
+            }
         }
 
-        public Task<BTUser> GetProjectManagerAsync(int projectId)
+        public async Task<BTUser> GetProjectManagerAsync(int projectId)
         {
-            throw new NotImplementedException();
+            try
+            {
+            }
+            catch (Exception)
+            {
+                throw;
+            }
         }
 
-        public Task<List<BTUser>> GetProjectMembersByRoleAsync(int projectId, string role)
+        public async Task<List<BTUser>> GetProjectMembersByRoleAsync(int projectId, string role)
         {
-            throw new NotImplementedException();
+            try
+            {
+            }
+            catch (Exception)
+            {
+                throw;
+            }
         }
 
-        public Task<List<BTUser>> GetSubmittersOnProjectAsync(int projectId)
+        public async Task<List<BTUser>> GetSubmittersOnProjectAsync(int projectId)
         {
-            throw new NotImplementedException();
+            try
+            {
+            }
+            catch (Exception)
+            {
+                throw;
+            }
         }
 
-        public Task<List<Project>> GetUserProjectsAsync(string userId)
+        public async Task<List<Project>> GetUserProjectsAsync(string userId)
         {
-            throw new NotImplementedException();
+            try
+            {
+            }
+            catch (Exception)
+            {
+                throw;
+            }
         }
 
-        public Task<List<BTUser>> GetUsersNotOnProjectAsync(int projectId, int companyId)
+        public async Task<List<BTUser>> GetUsersNotOnProjectAsync(int projectId, int companyId)
         {
-            throw new NotImplementedException();
+            try
+            {
+            }
+            catch (Exception)
+            {
+                throw;
+            }
         }
 
-        public Task<bool> IsUserOnProject(string userId, int projectId)
+        public async Task<bool> IsUserOnProject(string userId, int projectId)
         {
-            throw new NotImplementedException();
+            try
+            {
+            }
+            catch (Exception)
+            {
+                throw;
+            }
         }
 
-        public Task<int> LookupProjectPriorityId(string priorityName)
+        public async Task<int> LookupProjectPriorityId(string priorityName)
         {
-            throw new NotImplementedException();
+            try
+            {
+            }
+            catch (Exception)
+            {
+                throw;
+            }
         }
 
-        public Task RemoveProjectManagerAsync(int projectId)
+        public async Task RemoveProjectManagerAsync(int projectId)
         {
-            throw new NotImplementedException();
+            try
+            {
+            }
+            catch (Exception)
+            {
+                throw;
+            }
         }
 
-        public Task RemoveUserFromProjectAsync(string userId, int projectId)
+        public async Task RemoveUserFromProjectAsync(string userId, int projectId)
         {
-            throw new NotImplementedException();
+            try
+            {
+            }
+            catch (Exception)
+            {
+                throw;
+            }
         }
 
-        public Task RemoveUsersFromProjectByRoleAsync(string role, int projectId)
+        public async Task RemoveUsersFromProjectByRoleAsync(string role, int projectId)
         {
-            throw new NotImplementedException();
+            try
+            {
+            }
+            catch (Exception)
+            {
+                throw;
+            }
         }
 
-        public Task UpdateProjectAsync(Project project)
+        public async Task UpdateProjectAsync(Project project)
         {
-            throw new NotImplementedException();
+            try
+            {
+            }
+            catch (Exception)
+            {
+                throw;
+            }
         }
     }
 }
