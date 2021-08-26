ee0d5b7d8514887cafb3957af3ad73940c98e7da
elbiesmith <elbie.smith@gmail.com>
2021-08-26 16:07:39 -0500

    Update BTProjectService.cs

--- Services/BTProjectService.cs
+++ Services/BTProjectService.cs
@@ -245,6 +245,7 @@ namespace Titan_BugTracker.Services
             {
                 Project project = await _context.Projects
                                                 .Include(p => p.Tickets)
+                                                 .ThenInclude(t => t.TicketPriority)
                                                 .Include(p => p.Members)
                                                 .Include(p => p.ProjectPriority)
                                                 .FirstOrDefaultAsync(p => p.Id == projectId && p.CompanyId == companyId);
