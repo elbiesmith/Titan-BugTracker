9d901ad4763186129d956f7c83a78840ba2d120a
elbiesmith <elbie.smith@gmail.com>
2021-08-27 13:41:47 -0500

    Update BTNotificationService.cs

--- Services/BTNotificationService.cs
+++ Services/BTNotificationService.cs
@@ -21,17 +21,32 @@ namespace Titan_BugTracker.Services
             _rolesService = rolesService;
         }
 
-        public Task AddHistoryAsync(Ticket oldTicket, Ticket newTicket, string userId)
+        public Task AddNotificationAsync(Notification notification)
         {
             throw new NotImplementedException();
         }
 
-        public Task<List<TicketHistory>> GetCompanyTicketsHistoriesAsync(int companyId)
+        public Task<List<Notification>> GetReceivedNotificationsAsync(string userId)
         {
             throw new NotImplementedException();
         }
 
-        public Task<List<TicketHistory>> GetProjectTicketsHistoriesAsync(int projectId, int companyId)
+        public Task<List<Notification>> GetSentNotificationsAsync(string userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> SendEmailNotificationAsync(Notification notification, string emailSubject)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task SendEmailNotificationsByRoleAsync(Notification notification, int companyId, string role)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task SendMembersEmailNotificationsAsync(Notification notification, List<BTUser> members)
         {
             throw new NotImplementedException();
         }
