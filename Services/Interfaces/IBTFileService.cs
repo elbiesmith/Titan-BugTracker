a01387b20725c4aa4112b3380f9f8ab62cbc5a3a
elbiesmith <elbie.smith@gmail.com>
2021-08-27 13:36:04 -0500

    Create IBTFileService.cs

---
+++ Services/Interfaces/IBTFileService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Titan_BugTracker.Services.Interfaces
+{
+    public interface IBTFileService
+    {
+        public Task<byte[]> ConvertFileToByteArrayAsync(IFormFile file);
+
+        public string ConvertByteArrayToFile(byte[] fileData, string extension);
+
+        public string GetFileIcon(string file);
+
+        public string FormatFileSize(long bytes);
+    }
+}
