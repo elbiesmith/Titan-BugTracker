34090311b31e7f3c663e9886a4845237a70cbb55
elbiesmith <elbie.smith@gmail.com>
2021-08-27 13:31:56 -0500

    added notification service and history interface

---
+++ Services/Interfaces/IBTHistoryService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Titan_BugTracker.Models;
+
+namespace Titan_BugTracker.Services.Interfaces
+{
+    public interface IBTHistoryService
+    {
+        Task AddHistoryAsync(Ticket oldTicket, Ticket newTicket, string userId);
+
+        Task<List<TicketHistory>> GetProjectTicketsHistoriesAsync(int projectId, int companyId);
+
+        Task<List<TicketHistory>> GetCompanyTicketsHistoriesAsync(int companyId);
+    }
+}
