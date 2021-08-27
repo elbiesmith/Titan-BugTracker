fa2351ea4509239dc0e3bcdc9b6c7236242f5100
elbiesmith <elbie.smith@gmail.com>
2021-08-27 13:40:20 -0500

    Update BTInviteService.cs

--- Services/BTInviteService.cs
+++ Services/BTInviteService.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Titan_BugTracker.Data;
+using Titan_BugTracker.Models;
 using Titan_BugTracker.Services.Interfaces;
 
 namespace Titan_BugTracker.Services
@@ -14,29 +15,27 @@ namespace Titan_BugTracker.Services
         private readonly IEmailSender _emailSender;
         private readonly IBTProjectService _projectService;
 
-        public BTInviteService(ApplicationDbContext context, IEmailSender emailSender, IBTProjectService projectService)
-        {
-            _context = context;
-            _emailSender = emailSender;
-            _projectService = projectService;
-        }
-
-        public string ConvertByteArrayToFile(byte[] fileData, string extension)
+        public Task<bool> AcceptInviteAsync(Guid? token, string userId)
         {
             throw new NotImplementedException();
         }
 
-        public Task<byte[]> ConvertFileToByteArrayAsync(IFormFile file)
+        public Task<bool> AnyInviteAsync(Guid token, string email)
         {
             throw new NotImplementedException();
         }
 
-        public string FormatFileSize(long bytes)
+        public Task<Invite> GetInviteAsync(int id)
         {
             throw new NotImplementedException();
         }
 
-        public string GetFileIcon(string file)
+        public Task<Invite> GetInviteAsync(Guid token, string email)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> ValidateInviteCodeAsync(Guid? token)
         {
             throw new NotImplementedException();
         }
