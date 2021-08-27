bfcbd645f8c008940c782bb61abb1beef9aef728
elbiesmith <elbie.smith@gmail.com>
2021-08-27 13:36:20 -0500

    Update IBTInviteService.cs

--- Services/Interfaces/IBTInviteService.cs
+++ Services/Interfaces/IBTInviteService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Http;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,5 +8,12 @@ namespace Titan_BugTracker.Services.Interfaces
 {
     public interface IBTInviteService
     {
+        public Task<byte[]> ConvertFileToByteArrayAsync(IFormFile file);
+
+        public string ConvertByteArrayToFile(byte[] fileData, string extension);
+
+        public string GetFileIcon(string file);
+
+        public string FormatFileSize(long bytes);
     }
 }
