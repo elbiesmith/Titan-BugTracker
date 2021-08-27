c80587b36746d5273070a34e2936639e04d233e4
elbiesmith <elbie.smith@gmail.com>
2021-08-27 13:27:14 -0500

    Create BTInviteService.cs

---
+++ Services/BTInviteService.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Titan_BugTracker.Data;
+using Titan_BugTracker.Services.Interfaces;
+
+namespace Titan_BugTracker.Services
+{
+    public class BTInviteService : IBTInviteService
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly IEmailSender _emailSender;
+        private readonly IBTProjectService _projectService;
+
+        public BTInviteService(ApplicationDbContext context, IEmailSender emailSender, IBTProjectService projectService)
+        {
+            _context = context;
+            _emailSender = emailSender;
+            _projectService = projectService;
+        }
+
+        public string ConvertByteArrayToFile(byte[] fileData, string extension)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<byte[]> ConvertFileToByteArrayAsync(IFormFile file)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string FormatFileSize(long bytes)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetFileIcon(string file)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
