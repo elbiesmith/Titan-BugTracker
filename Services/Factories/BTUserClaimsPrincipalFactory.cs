fbde26be0600e16619409a33edda16e071ea3836
elbiesmith <elbie.smith@gmail.com>
2021-08-30 13:46:50 -0500

    Update BTUserClaimsPrincipalFactory.cs

--- Services/Factories/BTUserClaimsPrincipalFactory.cs
+++ Services/Factories/BTUserClaimsPrincipalFactory.cs
@@ -1,7 +1,9 @@
 ﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Options;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
 using Titan_BugTracker.Models;
 
@@ -9,5 +11,19 @@ namespace Titan_BugTracker.Services.Factories
 {
     public class BTUserClaimsPrincipalFactory : UserClaimsPrincipalFactory<BTUser, IdentityRole>
     {
+        public BTUserClaimsPrincipalFactory(UserManager<BTUser> userManager,
+                                            RoleManager<IdentityRole> roleManager,
+                                            IOptions<IdentityOptions> optionsAccessor)
+                                            : base(userManager, roleManager, optionsAccessor)
+        {
+        }
+
+        protected override async Task<ClaimsIdentity> GenerateClaimsAsync(BTUser user)
+        {
+            ClaimsIdentity identity = await base.GenerateClaimsAsync(user);
+            identity.AddClaim(new Claim("CompanyId", user.CompanyId.ToString()));
+
+            return identity;
+        }
     }
 }
