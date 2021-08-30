31fa3273daa4aaec45927c2462fcd2318eb9bd15
elbiesmith <elbie.smith@gmail.com>
2021-08-30 13:28:38 -0500

    Update BTUserClaimsPrincipalFactory.cs

--- Services/Factories/BTUserClaimsPrincipalFactory.cs
+++ Services/Factories/BTUserClaimsPrincipalFactory.cs
@@ -1,11 +1,13 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Identity;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Titan_BugTracker.Models;
 
 namespace Titan_BugTracker.Services.Factories
 {
-    public class BTUserClaimsPrincipalFactory
+    public class BTUserClaimsPrincipalFactory : UserClaimsPrincipalFactory<BTUser, IdentityRole>
     {
     }
 }
