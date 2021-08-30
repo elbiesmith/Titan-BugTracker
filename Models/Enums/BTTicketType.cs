9a26aa7b238951335ef1b54a1f3af338d698de72
elbiesmith <elbie.smith@gmail.com>
2021-08-30 15:58:20 -0500

    Update BTTicketType.cs

--- Models/Enums/BTTicketType.cs
+++ Models/Enums/BTTicketType.cs
@@ -7,5 +7,11 @@ namespace Titan_BugTracker.Models.Enums
 {
     public enum BTTicketType
     {
+        NewDevelopment,
+        WorkTask,
+        Defect,
+        ChangeRequest,
+        Enhancement,
+        GeneralTask
     }
 }
