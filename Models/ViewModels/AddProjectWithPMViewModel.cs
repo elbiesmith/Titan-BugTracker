bc56f02357c664cdf691a29d43db676caf3a5282
elbiesmith <elbie.smith@gmail.com>
2021-09-02 13:18:55 -0500

    Create AddProjectWithPMViewModel.cs

---
+++ Models/ViewModels/AddProjectWithPMViewModel.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Titan_BugTracker.Models.ViewModels
+{
+    public class AddProjectWithPMViewModel
+    {
+        public Project project { get; set; } = new();
+        public SelectList PMList { get; set; }
+        public string PmId { get; set; }
+        public SelectList PriorityList { get; set; }
+        public int ProjectPriority { get; set; }
+    }
+}
