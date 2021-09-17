54b89e996068658fc34cefd6cc837e2278d26d09
elbiesmith <elbie.smith@gmail.com>
2021-09-17 09:27:45 -0500

    Update AssignDeveloperViewModel.cs

--- Models/ViewModels/AssignDeveloperViewModel.cs
+++ Models/ViewModels/AssignDeveloperViewModel.cs
@@ -8,6 +8,7 @@ namespace Titan_BugTracker.Models.ViewModels
 {
     public class AssignDeveloperViewModel
     {
+        public Project Project { get; set; }
         public SelectList Developers { get; set; }
         public string DeveloperId { get; set; }
         public Ticket Ticket { get; set; }
